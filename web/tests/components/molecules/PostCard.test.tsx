a340ea167f0d6c516058894566e16efb19a8611e
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2025-11-22 19:07:09 +0900

    refactor: move postcard test to tests directory

--- web/tests/components/molecules/PostCard.test.tsx
+++ web/tests/components/molecules/PostCard.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { PostCard } from './index';
+import { PostCard } from '../../../components/molecules/PostCard';
 import { describe, it, expect } from 'vitest';
 
 describe('PostCard', () => {

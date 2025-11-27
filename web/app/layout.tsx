a930ee37612b41627bb3ffd0a3d0cc0b684ae29f
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2025-11-27 21:51:01 +0900

    GTMの設定を追加

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleTagManager gtmId="GTM-PHFW2HWB" />
     </html>
   );
 }

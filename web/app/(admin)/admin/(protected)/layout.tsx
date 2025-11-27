bcd1f77f760d1b6405c8ffd466052184209c4806
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2025-11-27 10:02:50 +0900

    セキュリティ強化

---
+++ web/app/(admin)/admin/(protected)/layout.tsx
@@ -0,0 +1,17 @@
+import { auth } from '../../../../auth';
+import { redirect } from 'next/navigation';
+import React from 'react';
+
+export default async function ProtectedAdminLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const session = await auth();
+
+    if (!session) {
+        redirect('/admin/login');
+    }
+
+    return <>{children}</>;
+}

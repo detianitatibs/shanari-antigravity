fa6ee5d7893d4939c16a3f7ef59b44c1e9de9ba2
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2025-11-30 15:31:42 +0900

    コンテンツの表示幅を少し狭くするように修正

--- web/components/templates/DefaultLayout/index.tsx
+++ web/components/templates/DefaultLayout/index.tsx
@@ -11,7 +11,7 @@ export const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children }) => {
         <div className="flex min-h-screen flex-col bg-zinc-50">
             <Header />
             <main className="flex-grow">
-                <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-6xl px-4 py-8 sm:px-6 lg:px-8">
                     {children}
                 </div>
             </main>
